b924cf63064f27e78c557f71e09c6c147ca75642
kingxu <kingxu@smail.nju.edu.cn>
2019-09-17 22:58:32 +0800

    Add more function

--- repo_process.py
+++ repo_process.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import json
 import re
+import time
 from pygments import highlight
 from pygments.lexers import JavaLexer
 from pygments.formatters import RawTokenFormatter
@@ -19,6 +20,8 @@ def get_file_contents_by_hash(hash_code):
     Returns:
         str -- 包含整个文件的字符串
     """
+    # cmd = 'git cat-file -p ' + hash_code
+    # child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     child = subprocess.Popen(['git', 'cat-file', '-p', hash_code],
                              stdout=subprocess.PIPE)
     ret = child.stdout.read().decode('utf-8', 'ignore')
@@ -37,6 +40,8 @@ def get_commits_in_repo(repo_dir='./'):
     Returns:
         list -- 由commit组成的list.
     """
+    if not os.path.exists(repo_dir):
+        return None
     os.chdir(repo_dir)
     child1 = subprocess.Popen(['git', 'log', '-p', '--no-merges'],
                               stdout=subprocess.PIPE)
@@ -90,13 +95,70 @@ def get_commits_in_repo(repo_dir='./'):
             index = line.split(' ')[1]
             indexs = index.split('..')
             if len(indexs) == 2:
-                file1 = file1 + '\t' + indexs[0]
-                file2 = file2 + '\t' + indexs[1]
+                file1 = file1 + '\t' + indexs[0].strip(' \r\n\t')
+                file2 = file2 + '\t' + indexs[1].strip(' \r\n\t')
         line = child1.stdout.readline().decode('utf-8', 'ignore')
-    repo = repo_dir.split('/')[-1]
+    repo = repo_dir.strip('/').split('/')[-1]
     json.dump(commits, open("/mnt/data1/kingxu/{}.commits.json".format(repo),
                             'w'))
     return commits
+
+
+def extract_files(json_file, repo_base, base_path):
+    """ 为某个项目的commits提取所有对应的java文件,文件存放在对应文件夹中,
+        文件名为短hash值,因为一个项目中的hash值是唯一的.
+
+    Arguments:
+        json_file {string} -- 文件名,以'.commits.json'结尾
+        repo_base {string} -- repo所在的文件夹,不需要指定repo,从json_file获得
+        base_path {string} -- 指定的存java文件的文件夹所在的路径
+    """
+    commits = json.load(open(json_file, 'r'), encoding='utf-8')
+    json_file = json_file.split('/')[-1]
+    dir_name = json_file[:-13]
+    repo_dir = os.path.join(repo_base, dir_name)
+    dir = os.path.join(base_path, dir_name)
+    hash_set = set()
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    for commit in commits:
+        for file_pair in commit['files']:
+            file1, file2 = file_pair['file1'], file_pair['file2']
+            l1, l2 = file1.split('\t'), file2.split('\t')
+            if len(l1) == 2 and len(l2) == 2:
+                hash_set.add(l1[1])
+                hash_set.add(l2[1])
+    os.chdir(repo_dir)
+    for i in list(hash_set):
+        f_content = get_file_contents_by_hash(i)
+        if f_content:
+            f_name = os.path.join(dir, '{}.java'.format(i))
+            with open(f_name, 'w') as f:
+                f.write(f_content)
+
+
+def extract_class_and_method(java_dir, base_path):
+    if not os.path.isdir(java_dir):
+        return None
+    files = os.listdir(java_dir)
+    names_dict = {}
+    for f in files:
+        h, i = os.path.splitext(f)
+        if i == '.java':
+            names = set()
+            with open(os.path.join(java_dir, f)) as fl:
+                cont = fl.read()
+                x = highlight(cont, JavaLexer(), RawTokenFormatter())
+                for y in str(x, encoding='utf-8').splitlines():
+                    ys = y.split('\t')
+                    if ys[0].startswith('Token.Name.Class') or \
+                            ys[0] != 'Token.Name.Function':
+                        names.add(eval(ys[1]))
+            names_dict[h] = list(names)
+    repo = java_dir.strip('/').split('/')[-1]
+    jf = os.path.join(base_path, '{}.names.json'.format(repo))
+    json.dump(names_dict, open(jf, 'w'))
+    return names_dict
 
 
 def write_files_in_directory(commits, base_path):
